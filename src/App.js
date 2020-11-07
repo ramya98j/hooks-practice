48f6868e4d09905b90bb6cbe3c6b936c5d257a2e
ramya vummaneni <ramyavummaneni@ramyas-MacBook-Pro.local>
2020-11-07 15:18:52 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
 import logo from './logo.svg';
 import './App.css';
+import bootstrap from '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import React ,{ useState } from 'react';
 
-function App() {
+const App =() =>{
+  const[task,setTask]=useState('');
+  const[tasks,setTasks]=useState([]);
+
+
+  const addTask =()=>{
+    setTasks([...tasks,task]);
+    setTask('');
+  }
+  
+
+  const deleteTask=(index)=>{
+    var duplicatearray=[...tasks];
+    duplicatearray.splice(index,1);
+    setTasks(duplicatearray);
+    
+
+  }
+
+  const list= tasks.map((obj,index)=>{
+
+    return (
+      <div className="row justify-content-center">
+        <h1 className="col-md-6 text-left">{index+1}.{obj}</h1>
+        <button className="btn btn-danger m-1" onClick={()=>{deleteTask(index)}}>Delete</button>
+      </div>
+    )
+
+  })
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+      <div className="App">
+        <h1>React Todo List</h1>
+        <div className="row justify-content-center">
+        <input style={{backgroundColor:'lightsalmon'}}type="text" 
+        placeholder="Enter task" 
+        className="form-control col-md-6 m-1" 
+        value={task}
+        onChange={(e)=>{setTask(e.target.value)}}/>
+        <button onClick={addTask} className="btn btn-primary col-md-1 m-1">Add</button>
+        
+        
+        </div>
+        {list}
+        </div>
+
+    
+  
   );
-}
+};
 
 export default App;
